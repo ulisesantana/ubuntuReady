b8b2021fe4dd2baf3fb340039ded42f41dd080f7
Ulises Santana <ulisesantana@gmail.com>
2018-03-31 10:28:48 +0100

    Fix docker install

--- ubuntuReady.sh
+++ ubuntuReady.sh
@@ -109,7 +109,18 @@ sudo apt-get install gimp -y
 sudo apt-get install git -y
 
 ##Install docker
-sudo apt-get install -y docker-engine
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
 sudo usermod -aG docker $USER # Enable use docker without sudo
 
 ##Install Node Version Manager
